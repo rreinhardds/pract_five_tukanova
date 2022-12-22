5e787dbc1460f21ccbda02568445dfe20e013e57
kolyasnikovpr22 <deidara275071>
2022-12-22 16:38:31 +0500

    first commit

---
+++ первый файл/задание1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private List<SolidColorBrush> brushes;
+        private Random rnd; 
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Автор: Туканова Елизавета", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void MenuItem_Click_exit(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            rnd = new Random();
+            brushes = new List<SolidColorBrush> 
+            {
+              Brushes.LawnGreen,
+              Brushes.Pink,
+              Brushes.Violet,
+              Brushes.Aquamarine
+              };
+            window.Background = brushes[rnd.Next(brushes.Count)];
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            window.Background = new SolidColorBrush(Colors.Yellow);
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            window.Background = new SolidColorBrush(Colors.Green);
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            window.Background = new SolidColorBrush(Colors.Red);
+        }
+    }
+}
---
+++ первый файл/задание2/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ComboBoxItem_Selected_Red(object sender, RoutedEventArgs e)
+        {
+            InCnvs.DefaultDrawingAttributes.Color = Colors.Red;
+        }
+        private void ComboBoxItem_Selected_Green(object sender, RoutedEventArgs e)
+        {
+            InCnvs.DefaultDrawingAttributes.Color = Colors.Green;
+        }
+        private void ComboBoxItem_Selected_Blue(object sender, RoutedEventArgs e)
+        {
+            InCnvs.DefaultDrawingAttributes.Color = Colors.Blue;
+        }
+        private void ComboBoxItem_Selected_Purple(object sender, RoutedEventArgs e)
+        {
+            InCnvs.DefaultDrawingAttributes.Color = Colors.Purple;
+        }
+
+        private void ComboBoxItem_Selected_Yellow(object sender, RoutedEventArgs e)
+        {
+            InCnvs.DefaultDrawingAttributes.Color = Colors.Yellow;
+        }
+
+        private void ComboBoxItem_Selected_Grey(object sender, RoutedEventArgs e)
+        {
+            InCnvs.DefaultDrawingAttributes.Color = Colors.Gray;
+        }
+
+
+        private void ComboBoxItem_Selected_BlueLig(object sender, RoutedEventArgs e)
+        {
+            InCnvs.DefaultDrawingAttributes.Color = Colors.LightBlue;
+        }
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ((Slider)sender).SelectionEnd = e.NewValue;
+            if (sizeLable == null) return;
+            sizeLable.Text = slider.Value.ToString();
+            InCnvs.DefaultDrawingAttributes.Width = slider.Value;
+            InCnvs.DefaultDrawingAttributes.Height = slider.Value;
+        }
+        private void RadioButton_Boot3_Click_Edit(object sender, RoutedEventArgs e)
+        {
+            InCnvs.EditingMode = InkCanvasEditingMode.EraseByPoint;
+        }
+        private void RadioButton_Boot3_Click_Delete(object sender, RoutedEventArgs e)
+        {
+            InCnvs.Strokes.Clear();
+        }
+
+        private void Rb1_Click(object sender, RoutedEventArgs e)
+        {
+            InCnvs.EditingMode = InkCanvasEditingMode.InkAndGesture;
+        }
+    }
+}
